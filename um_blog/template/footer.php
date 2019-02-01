cd344b64e2aa2360cbb0e77f78733ef0a5c1fa6c
20154530 <2483507105@qq.com>
2019-02-01 12:46:05 +0800

    响应式布局

---
+++ um_blog/template/footer.php
@@ -0,0 +1,10 @@
+{* Template Name:公共底部 *}
+</div></section><div class="clear"></div>
+<div id="divBottom">
+    <div class="text"><h4 id="BlogPowerBy">Powered By {$zblogphphtml}</h4><span>&nbsp;Theme By <a href="http://www.umhtml.com" title="优美尚品" target="_blank">优美尚品</a></span></div>
+    <div class="text"><h3 id="BlogCopyRight">{$copyright}</h3>&nbsp;<span>{$zbp->Config('um_blog')->beian}</span></div>
+</div>
+{$zbp->Config('um_blog')->ftcode}
+{$footer}
+</body>
+</html>
